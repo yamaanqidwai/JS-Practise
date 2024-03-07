c8da22afe3a93485528b58e8dddfffd2aada9bf0
Yamaan-Ahmed-Qidwai <143816522+yamaanqidwai@users.noreply.github.com>
2024-03-08 04:13:03 +0500

    completed chapter 3

--- Chapter-3/app.js
+++ Chapter-3/app.js
@@ -1,3 +1,25 @@
-var species = "what is your species?";
-var spec_default = "human";
-var spec_ques = prompt(species, spec_default);
+var marks = 69; // declared variable named marks and assigned 69 to it // data type is number
+
+var new_marks = marks + 11; // added 11 to marks value // new_marks is 80
+
+console.log(marks);
+console.log(new_marks);
+
+var marks2 = "69"; // data type is string
+
+console.log(marks2);
+
+var orig_num = 90
+orig_num = orig_num + 10 // incrementing the number by 10
+
+console.log(orig_num)
+
+var num = "7"
+var num2 = 23
+var num_total = num+num2
+
+console.log(num_total) // instead of adding together, js concatenated both variables
+
+var num = 144
+alert(num) // although alert should output in number data type, it outputs as string
+
