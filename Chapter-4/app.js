ee42196a6a5b2e64770541bf20b518c046d163a3
Yamaan-Ahmed-Qidwai <143816522+yamaanqidwai@users.noreply.github.com>
2024-03-08 05:03:07 +0500

    completed chapter 3

---
+++ Chapter-4/app.js
@@ -0,0 +1,33 @@
+// Variable Naming
+
+/*  No spaces
+---------------------
+    var my Name
+        ^^^^^^^
+*/
+
+var myName;
+
+// Only letters, numbers, dollar signs, and underscores
+
+var kil1$_yourself;
+
+// Though a variable name can't be any of JavaScript's keywords, it can contain keywords.
+
+var myalert;
+var $var;
+
+
+// Naming Conventions
+// -----------------------------------
+// camelCase
+
+var camelCase;
+
+// PascalCase
+
+var PascalCase;
+
+// underscore
+
+var underscore_var;
