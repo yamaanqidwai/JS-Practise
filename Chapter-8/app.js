fbca70b090730c7d79606efee1367fa1f43bad50
Yamaan-Ahmed-Qidwai <143816522+yamaanqidwai@users.noreply.github.com>
2024-03-10 01:50:01 +0500

    completed chapter 8

---
+++ Chapter-8/app.js
@@ -0,0 +1,20 @@
+let me = "be myself";
+let you = "be yourself";
+
+console.log("let me " + me + " and you " + you);
+
+var g = "good";
+var b = "bye";
+var w = "world";
+
+console.log(g + b + " " + w);
+
+var complete = g + b + " " + w;
+
+
+console.log(g + b + " " + w);
+
+var num1 = "2";
+var num2 = 2;
+
+console.log(num1 + num2);
