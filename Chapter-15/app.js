1b033a64b2c4ecdccc54bce6f76a9813feab656f
Yamaan-Ahmed-Qidwai <143816522+yamaanqidwai@users.noreply.github.com>
2024-03-16 16:27:47 +0500

    completed chapter 15

---
+++ Chapter-15/app.js
@@ -0,0 +1,13 @@
+// Declaration of Array
+// var myArray = [x , y , z]
+
+var cities = ["Karachi", "Lahore", "Islamabad", "Hyderabad", "Peshawar"];
+
+// index starts from 0        IMPORTANT
+
+// In order to 'call' the items of the array this format will be used ===>  arrayName[index]
+console.log(cities[0]); //Karachi
+console.log(cities[1]); //Lahore
+console.log(cities[2]); //Islamabad
+console.log(cities[3]); //Hyderabad
+console.log(cities[4]); //Peshawar
